7565540cf5854b6b5269fcc98588cc0adf03b9f6
saatvikruddra <saatrud95@gmail.com>
2018-11-17 03:26:49 +0530

    All done..only Full Loop left

--- traffic.js
+++ traffic.js
@@ -1,65 +1,132 @@
-
-    var red1 = document.getElementById("red")
-    var red2 = document.getElementById("red1")
-    var red3 = document.getElementById("red2")
-    var red4 = document.getElementById("red3")
-
-    var yellow1 = document.getElementById("yellow")
-    var yellow2 = document.getElementById("yellow1")
-    var yellow3 = document.getElementById("yellow2")
-    var yellow4 = document.getElementById("yellow3")
     
-    var green1 = document.getElementById("green")
-    var green2 = document.getElementById("green1")
-    var green3 = document.getElementById("green2")
-    var green4 = document.getElementById("green3")
+    let red1 = document.getElementById("red")
+    let red2 = document.getElementById("red1")
+    let red3 = document.getElementById("red2")
+    let red4 = document.getElementById("red3")
 
-    red1.style.opacity=".1";
-    yellow1.style.opacity=".1";
-    green1.style.opacity=".1";
+    let yellow1 = document.getElementById("yellow")
+    let yellow2 = document.getElementById("yellow1")
+    let yellow3 = document.getElementById("yellow2")
+    let yellow4 = document.getElementById("yellow3")
 
-var top = setInterval(colorChangeRed, 300);
-    
-    function colorChangeRed(){ 
-        red1.style.opacity="1";
-        yellow1.style.opacity="1";
-        green1.style.opacity="1";    
-        // red2.style.backgroundColor = "red"
-        // red3.style.backgroundColor = "red"
-        // red4.style.backgroundColor = "black"
-        // green4.style.backgroundColor = "green"
+    let green1 = document.getElementById("green")
+    let green2 = document.getElementById("green1")
+    let green3 = document.getElementById("green2")
+    let green4 = document.getElementById("green3")
+
+    // Default Values Set
+    green1.style.opacity = "1";
+    red2.style.opacity = "1";
+    red3.style.opacity = "1";
+    red4.style.opacity = "1";
+
+    ///////////////////////////
+    // Dedicted to Red and Green
+    ///////////////////////////
+
+    initialStage=()=>{
+        green1.style.opacity=".1";
+        red2.style.opacity=".1";
+        red3.style.opacity=".1";
+        red4.style.opacity=".1"; 
     }
-// var middle = setTimeout(colorChangeYellow,1000);
-
-//     function colorChangeYellow(){
-
-//         green4.style.backgroundColor = "black" 
-//         yellow1.style.backgroundColor = "yellow"
-//         red1.style.backgroundColor = "black"
-//         // red1.style.backgroundColor = "red"
-//         yellow4.style.backgroundColor = "yellow"
+    
+    secondStage=()=>{
         
-//     }
-// var bottom = setTimeout(colorChangeGreen,5000);
-
-//     function colorChangeGreen(){ 
-//         yellow4.style.backgroundColor = "black";
-//         green4.style.backgroundColor = "black";
-//         red4.style.backgroundColor = "red";
-//         green1.style.backgroundColor= "green"
-//         yellow1.style.backgroundColor = "black" 
-//     }
-
-// red1.style.backgroundColor = "illuminatedRed";
-// red2.style.backgroundColor = "red";
-// red3.style.backgroundColor = "red";
-// green4.style.backgroundColor = "green";
-
-
-// var changeColor = setInterval(colors,1000)
-//     function colors(){
-//         red1.style.backgroundColor = "red" ? 
-//         green3.style.backgroundColor="green" : 
-         
+        red1.style.opacity="1";
+        green2.style.opacity="1";
+        red3.style.opacity="1";
+        red4.style.opacity="1";
+    }
+    
+    resetSecondStage=()=>{
         
-//     }
+        red1.style.opacity=".1";
+        green2.style.opacity=".1";
+        red3.style.opacity=".1";
+        red4.style.opacity=".1";
+    }
+    thirdStage=()=>{
+        
+        red1.style.opacity="1";
+        red2.style.opacity="1";
+        green3.style.opacity="1";
+        red4.style.opacity="1";    
+    }
+    resetThirdStage=()=>{
+        
+        red1.style.opacity=".1";
+        red2.style.opacity=".1";
+        green3.style.opacity=".1";
+        red4.style.opacity=".1";    
+    }
+    fourthStage=()=>{
+        
+        red1.style.opacity="1";
+        red2.style.opacity="1";
+        red3.style.opacity="1";
+        green4.style.opacity="1";    
+    }
+    resetFourthStage=()=>{
+        
+        red1.style.opacity=".1";
+        red2.style.opacity=".1";
+        red3.style.opacity=".1";
+        green4.style.opacity=".1";    
+        
+// Resetting to Default
+        green1.style.opacity="1";
+        red2.style.opacity="1";
+        red3.style.opacity="1";
+        red4.style.opacity="1";
+ 
+    }
+  
+    //////////////////////////
+    // Red and Green Twerking
+    //////////////////////////
+   let start = setTimeout(initialStage, 15000);
+    
+    let second = setTimeout(secondStage, 15000);
+    let resetSecond = setTimeout(resetSecondStage, 30000);
+
+    let third = setTimeout(thirdStage, 30000);
+    let resetThird = setTimeout(resetThirdStage, 45000);
+
+    let fourth = setTimeout(fourthStage, 45000);
+    let resetFourth = setTimeout(resetFourthStage, 60000);
+
+    ////////////////////////
+    // Yellow Light Twerking
+    ////////////////////////
+
+    yellowLight=(light)=>{
+        console.log(light);
+        light.style.opacity = "1";
+    }
+    resetYellowLight=(light)=>{
+        light.style.opacity = ".1";
+    }
+    let startYellow1a = setTimeout(yellowLight, 10000,yellow1);
+    let resetYellow1a = setTimeout(resetYellowLight, 15000,yellow1);
+
+    let startYellow2a = setTimeout(yellowLight, 10000,yellow2);
+    let resetYellow2a = setTimeout(resetYellowLight, 15000,yellow2);
+
+    let startYellow2b = setTimeout(yellowLight, 25000,yellow2);
+    let resetYellow2b = setTimeout(resetYellowLight, 30000,yellow2);
+
+    let startYellow3a = setTimeout(yellowLight, 25000,yellow3);
+    let resetYellow3a = setTimeout(resetYellowLight, 30000,yellow3);
+
+    let startYellow3b = setTimeout(yellowLight, 40000,yellow3);
+    let resetYellow3b = setTimeout(resetYellowLight, 45000,yellow3);
+
+    let startYellow4a = setTimeout(yellowLight, 40000,yellow4);
+    let resetYellow4a = setTimeout(resetYellowLight, 45000,yellow4);
+
+    let startYellow4b = setTimeout(yellowLight, 55000,yellow4);
+    let resetYellow4b = setTimeout(resetYellowLight, 60000,yellow4);
+
+    let startYellow1b = setTimeout(yellowLight, 55000,yellow1);
+    let resetYellow1b = setTimeout(resetYellowLight, 60000,yellow1);
